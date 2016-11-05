8842c476e36d8385d7078995cf79f4688ef75c16
jchan3 <joechan2014@gmail.com>
2016-11-05 05:09:14 +0000

    task 03

---
+++ task_03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for task_03."""
+
+
+import produce
+
+
+class Apple(produce.Produce):
+    """A subclass of the produce class.
+
+    Args:
+        None
+
+    Attributes:
+        duration (numeric): The duration of the Apple object.
+    """
+    duration = 5356800
