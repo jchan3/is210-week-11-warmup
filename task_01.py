303d9a3c2d3c64856bfb219f3e4e07d7396e2803
jchan3 <joechan2014@gmail.com>
2016-11-05 04:50:22 +0000

    task 01

---
+++ task_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for task_01."""
+
+
+import produce
+
+
+TOMATO = produce.Produce()
+EGGPLANT = produce.Produce(1311210802)
+
+TOMATO_ARRIVAL = TOMATO.arrival
+EGGPLANT_EXPIRES = EGGPLANT.get_expiration()
